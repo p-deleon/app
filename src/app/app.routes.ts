d27f05ef2c340d5ae394b3e0c71109ff72e3ee4f
Prado De Leon <Prado.code@outlook.com>
2025-01-13 20:21:10 -0400

    two

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { ChildtwoComponent } from './childtwo/childtwo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "one",component:ChildComponent},
+    {path: "two", component:ChildtwoComponent}
+];
---
+++ src/app/childtwo/childtwo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-childtwo',
+  imports: [],
+  templateUrl: './childtwo.component.html',
+  styleUrl: './childtwo.component.css'
+})
+export class ChildtwoComponent {
+
+}
